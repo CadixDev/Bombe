7384cc2949bcc495534a3f18fbb24a355f9e4594
Jamie Mansfield <jmansfield@cadixdev.org>
2021-01-02 22:33:45 +0000

    Replace Guava i/o copy with our own
    Thanks to @DemonWav for this implementation :)

--- bombe/src/main/java/org/cadixdev/bombe/util/ByteStreams.java
+++ bombe/src/main/java/org/cadixdev/bombe/util/ByteStreams.java
@@ -37,62 +37,37 @@ import java.io.OutputStream;
 /**
  * Utility for working with byte streams.
  *
- * @author Guava Authors
+ * @author Kyle Wood
  * @since 0.3.0
  */
 public final class ByteStreams {
 
-    // ------------------------------------------------
-    // The following code is taken from Guava (d0d5bd7)
-    //
-    // The code remains mostly untouched, only style-differences are present.
-    // ------------------------------------------------
-    // Copyright (C) 2007 The Guava Authors
-    //
-    // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
-    // in compliance with the License. You may obtain a copy of the License at
-    //
-    // http://www.apache.org/licenses/LICENSE-2.0
-    //
-    // Unless required by applicable law or agreed to in writing, software distributed under the License
-    // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-    // or implied. See the License for the specific language governing permissions and limitations under
-    // the License.
-    // ------------------------------------------------
-
-    private static final int BUFFER_SIZE = 8192;
-
-    /** Creates a new byte array for buffering reads or writes. */
-    private static byte[] createBuffer() {
-        return new byte[BUFFER_SIZE];
+    /**
+     * Copy all of the data from the {@code from} input to the {@code to} output.
+     *
+     * @param from The input to copy from.
+     * @param to The output to copy to.
+     * @param buffer The byte array to use as the copy buffer.
+     * @throws IOException If an IO error occurs.
+     */
+    public static void copy(final InputStream from, final OutputStream to, final byte[] buffer) throws IOException {
+        int read;
+        while ((read = from.read(buffer)) != -1) {
+            to.write(buffer, 0, read);
+        }
     }
 
     /**
-     * Copies all bytes from the input stream to the output stream. Does not close or flush either
-     * stream.
+     * Copy all of the data from the {@code from} input to the {@code to} output,
+     * using a default buffer.
      *
-     * @param from the input stream to read from
-     * @param to the output stream to write to
-     * @return the number of bytes copied
-     * @throws IOException if an I/O error occurs
+     * @param from The input to copy from.
+     * @param to The output to copy to.
+     * @throws IOException If an IO error occurs.
      */
-    public static long copy(final InputStream from, final OutputStream to) throws IOException {
-        final byte[] buf = createBuffer();
-        long total = 0;
-        while (true) {
-            final int r = from.read(buf);
-            if (r == -1) {
-                break;
-            }
-            to.write(buf, 0, r);
-            total += r;
-        }
-        return total;
+    public static void copy(final InputStream from, final OutputStream to) throws IOException {
+        copy(from, to, new byte[8192]);
     }
-
-    // ------------------------------------------------
-    // End of Apache-licensed Guava code.
-    // ------------------------------------------------
 
     private ByteStreams() {
     }
